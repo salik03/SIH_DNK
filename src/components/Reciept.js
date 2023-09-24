9535d5f810fdef73166211da80f0ee48d635dbff
salik03 <uddinsalik03@gmail.com>
2023-09-24 14:57:54 +0530

    Reciept page done

--- src/components/Reciept.js
+++ src/components/Reciept.js
@@ -1,62 +1,57 @@
-import React, { useState } from 'react';
+import { useRef, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import ReportTemplate from './ReportTemplate';
+import React from 'react';
 
-const Reciept = () => {
-  const [pdfContent, setPdfContent] = useState('');
-  const [downloadUrl, setDownloadUrl] = useState('');
+function Receipt() {
+  const reportTemplateRef = useRef(null);
 
-  const handleGeneratePdf = async () => {
-    const data = {
-      'INPUT_NAME': 'Salik',
-        'INPUT_PHONENUMBER': '123456',
-        'INPUT_COUNTRY': 'India',
-        'INPUT_ADDRESS': '123 MAin St',
-        'INPUT_RNAME': 'Avdhan',
-        'INPUT_RPHONENUMBER': '76542676',
-        'INPUT_RCOUNTRY': 'Rajasthan',
-        'INPUT_RADDRESS': '4332 Main St',
-        'INP_PT': 'Internation PArcel',
-        'INP_AN': 'efwfaew',
-        'INP_INS': '234234',
-        'INP_VALUE': '2342342',
-        'INP_WT': '23Kg',
-        'INP_QUANTITY': '50000'
+  const handlePrint = () => {
+    // Hide both buttons when printing
+    const printButton = document.querySelector('.button');
+    const backButton = document.querySelector('.back-button');
+
+    if (printButton) printButton.style.display = 'none';
+    if (backButton) backButton.style.display = 'none';
+
+    window.print();
+  };
+
+  useEffect(() => {
+    window.onbeforeprint = () => {
+      // Hide both buttons before printing
+      const printButton = document.querySelector('.button');
+      const backButton = document.querySelector('.back-button');
+
+      if (printButton) printButton.style.display = 'none';
+      if (backButton) backButton.style.display = 'none';
     };
 
-    try {
-      const response = await fetch('http://127.0.0.1:5000/generate_pdf', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
+    window.onafterprint = () => {
+      // Show both buttons after printing
+      const printButton = document.querySelector('.button');
+      const backButton = document.querySelector('.back-button');
 
-      if (response.ok) {
-        const responseData = await response.json();
-        setPdfContent(responseData.output_text);
-
-        const blob = new Blob([pdfContent], { type: 'application/pdf' });
-        const url = URL.createObjectURL(blob);
-        setDownloadUrl(url);
-      } else {
-        console.error('Error generating PDF:', response.status);
-      }
-    } catch (error) {
-      console.error('Error generating PDF:', error);
-    }
-  };
+      if (printButton) printButton.style.display = 'block';
+      if (backButton) backButton.style.display = 'block';
+    };
+  }, []);
 
   return (
     <div>
-      <button onClick={handleGeneratePdf}>Generate PDF</button>
-      {downloadUrl && (
-        <div>
-          <iframe title="Generated PDF" src={downloadUrl} width="100%" height="500px" />
-          <a href={downloadUrl} download="generated.pdf">Download PDF</a>
-        </div>
-      )}
+      <button className="button" onClick={handlePrint}>
+        Print
+      </button>
+      <Link to="/dashboard" className='dropdown-item'>
+        <button className="back-button">
+          Done
+        </button>
+      </Link>
+      <div ref={reportTemplateRef}>
+        <ReportTemplate />
+      </div>
     </div>
   );
-};
+}
 
-export default Reciept;
+export default Receipt;
